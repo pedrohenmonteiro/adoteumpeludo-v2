c8d516802391f87b540b0f54b8127ba05aa86235
Pedro Monteiro <pedromont436@gmail.com>
2025-01-28 20:09:33 -0300

    creating repository

---
+++ backend/src/main/java/com/projunifil/adoteumpeludo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.projunifil.adoteumpeludo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projunifil.adoteumpeludo.models.User;
+
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+    Optional<User> findByUsername(String username);
+}
