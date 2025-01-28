8c30cf0836dea76201b2f1f4f7c9535cce700418
Pedro Monteiro <pedromont436@gmail.com>
2025-01-27 23:27:36 -0300

    first commit

---
+++ backend/src/main/java/com/projunifil/adoteumpeludo/AdoteumpeludoApplication.java
@@ -0,0 +1,13 @@
+package com.projunifil.adoteumpeludo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteumpeludoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoteumpeludoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projunifil/adoteumpeludo/AdoteumpeludoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projunifil.adoteumpeludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteumpeludoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
