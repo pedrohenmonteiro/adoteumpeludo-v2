a4dfbb76763cbdf7cef7379d2a0f95f872f5a018
Pedro Monteiro <pedromont436@gmail.com>
2025-01-28 00:45:20 -0300

    updating user model

--- backend/src/main/java/com/projunifil/adoteumpeludo/models/User.java
+++ backend/src/main/java/com/projunifil/adoteumpeludo/models/User.java
@@ -1,5 +1,6 @@
 package com.projunifil.adoteumpeludo.models;
 
+import java.util.Locale;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -17,7 +18,7 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends AbstractAuditingEntity<Long> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -44,4 +45,55 @@ public class User {
 
     @JsonIgnore
     private Set<AuthorityEnum> authorities;
+
+
+     public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username.toLowerCase(Locale.ENGLISH);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Set<AuthorityEnum> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<AuthorityEnum> authorities) {
+        this.authorities = authorities;
+    }
+
+
 }
