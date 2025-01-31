eb8f0b8519a482295851c5d159701fb3aafd32dc
Pedro Monteiro <pedromont436@gmail.com>
2025-01-30 23:57:18 -0300

    implementing security jwt config

---
+++ backend/src/main/java/com/projunifil/adoteumpeludo/config/SecurityJwtConfig.java
@@ -0,0 +1,44 @@
+package com.projunifil.adoteumpeludo.config;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+
+@Configuration
+public class SecurityJwtConfig {
+
+    public static final MacAlgorithm JWT_ALGORITHM = MacAlgorithm.HS512;
+    
+    @Value("${authjwt.security.auth.jwt.base64-secret}")
+    private String jwtKey;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(getSecretKey()).macAlgorithm(JWT_ALGORITHM).build();
+        return token -> {
+                return jwtDecoder.decode(token);
+        };
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, JWT_ALGORITHM.getName());
+    }
+    
+}
