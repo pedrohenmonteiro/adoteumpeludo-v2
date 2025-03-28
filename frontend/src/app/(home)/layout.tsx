ac49d4a3a77e94b4cb5198bcb5e3ca415eccd066
Pedro Monteiro <pedro.monteiro@atos.net>
2025-03-28 16:45:45 -0300

    add home-navbar

---
+++ frontend/src/app/(home)/layout.tsx
@@ -0,0 +1,10 @@
+import { HomeNavbar } from "@/modules/home/ui/home-navbar";
+
+const HomeLayout = () => {
+    return ( <>
+    <HomeNavbar />
+    <p>Main...</p>
+    </> );
+}
+ 
+export default HomeLayout;
---
+++ frontend/src/app/(home)/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return ( <div>
+        Teste
+    </div> );
+}
+ 
+export default Home;
